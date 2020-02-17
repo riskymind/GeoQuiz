535db7d782ea13bc7409513584ecaa8620ba7c4c
kelechi <oparaiskelechi@gmail.com>
2020-02-17 23:51:57 +0100

    initial commit

---
+++ app/src/main/java/com/thisiskelechi/geoquiz/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.thisiskelechi.geoquiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var trueButton: Button
+    private lateinit var falseButton: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        trueButton = findViewById(R.id.true_button)
+        falseButton = findViewById(R.id.false_button)
+
+        trueButton.setOnClickListener { view: View ->
+            // Do something in response to the click here
+        }
+    }
+}
